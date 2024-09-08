65331551b0ba5e883a77a6f589844d9cc67feb12
markusedgar <markus@workplayexperience.com>
2024-09-08 23:41:16 +0200

    Change to Anthropic

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,35 @@
 import streamlit as st
-from openai import OpenAI
+import anthropic
 
-# Show title and description.
-st.title("📄 Document question answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+with st.sidebar:
+    anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("📝 File Q&A with Anthropic")
+
+uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
+
+question = st.text_input(
+    "Ask something about the article",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+if uploaded_file and question and not anthropic_api_key:
+    st.info("Please add your Anthropic API key to continue.")
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+if uploaded_file and question and anthropic_api_key:
+    article = uploaded_file.read().decode()
+    prompt = f"""{anthropic.HUMAN_PROMPT} Here's an article:\n\n
+    {article}\n\n\n\n{question}{anthropic.AI_PROMPT}"""
 
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+    client = anthropic.Client(api_key=anthropic_api_key)
+    response = client.completions.create(
+        prompt=prompt,
+        stop_sequences=[anthropic.HUMAN_PROMPT],
+        model="claude-v1", #"claude-2" for Claude 2 model
+        max_tokens_to_sample=100,
     )
-
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
-
-    if uploaded_file and question:
-
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
-
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
-        )
-
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+    st.write("### Answer")
+    st.write(response.completion)
